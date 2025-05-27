84fe97d79595d59d1aadc11b1fbf1e75e28144d5
aioverloadedVIV <mjvivek1997@gmail.com>
2025-05-27 23:23:38 +0530

    model trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,17 +1,17 @@
-#Data Ingestion refers to the process of collecting and importing data from various sources -
-#-into a storage system or processing framework where it can be accessed, used, and analyzed.
-
 import os
 import sys
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
-import numpy as np
+
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
@@ -27,7 +27,6 @@ class DataIngestion:
         try:
             df=pd.read_csv('notebook/data/study.csv')
             logging.info('Read the dataset as dataframe')
-
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
 
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
@@ -55,3 +54,6 @@ if __name__=="__main__":
 
     data_transformation=DataTransformation()
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
